30a9472aceaea0ceac1fea60e101f82cd5b00d13
Jose Baez <jose.baez.medina@gmail.com>
2016-02-05 16:44:42 -0400

    added devise user model and views

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -1,5 +1,10 @@
 class AwardsController < ApplicationController
+
+  before_filter :authenticate_user!
+
+
   def index
     @show_topbar = true
+    @logued_user = current_user.sent_messages.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'login/index'
 
   get 'mybets/index'
