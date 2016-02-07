94ed40506b9d272d6b58d9a64efd368bb99458ce
Jose Baez <jose.baez.medina@gmail.com>
2016-02-06 21:05:44 -0400

    added award creation functionality

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -1,10 +1,55 @@
 class AwardsController < ApplicationController
 
   before_filter :authenticate_user!
+  before_action :all_awards, only: [:index, :create]
+  respond_to :html, :js
 
 
   def index
     @show_topbar = true
-    #@logued_user = current_user.sent_messages.all
+    @award = Award.new 
+    @awards = Award.all
   end
+
+
+def new
+  @awards = Award.new
+end
+
+
+def all_awards
+  @awareds = Award.all
+end
+
+def award_params
+  params.require(:award).permit(:name, :description, :closing_date, :active)
+end
+
+ def create
+  #@awards = Award.all
+   @award = Award.create(award_params)
+ end
+
+
+def edit
+  @award = Award.find(params[:id])
+end
+
+def update
+  @awards = Award.all
+  @award = Award.find(params[:id])
+  @award.update_attributes(award_params)
+end
+
+def delete
+  @award = Award.find(params[:award_id])
+end
+
+def detroy
+  @awards = Award.all
+  @award = Award.find(params[:id])
+  @award.destroy
+end
+
+
 end
