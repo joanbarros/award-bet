584ab1dcf9787ca0ee2c9e3a2bb7582f1275f668
Jose Baez <jose.baez.medina@gmail.com>
2016-02-09 22:08:48 -0400

    added nominee model validations

--- app/models/nominee.rb
+++ app/models/nominee.rb
@@ -1,3 +1,9 @@
 class Nominee < ActiveRecord::Base
+
+
+  validates :name, presence: true, length: {minimum: 3}
+  validates :description, presence: true, length: {minimum: 3}
+  
+
   has_many :bets
 end
